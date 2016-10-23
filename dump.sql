833292ac9b7754261ef03ea82c72aba05fb0fc57
Alexandra Serebrennikova <sasha.serebrennik@gmail.com>
2016-10-23 23:58:50 +0300

    dump and jar 2.0

--- dump.sql
+++ dump.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `SingleTask` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE  IF NOT EXISTS `SingleTask` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `SingleTask`;
 -- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
 --
@@ -18,6 +18,63 @@ USE `SingleTask`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `Category`
+--
+
+DROP TABLE IF EXISTS `Category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `user` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_user_idx` (`user`),
+  KEY `category_parent_idx` (`parent`),
+  CONSTRAINT `category_parent` FOREIGN KEY (`parent`) REFERENCES `Category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `category_user` FOREIGN KEY (`user`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Task`
+--
+
+DROP TABLE IF EXISTS `Task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Task` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `comment` varchar(100) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `time` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `task_user_idx` (`user`),
+  CONSTRAINT `task_user` FOREIGN KEY (`user`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `TaskVariant`
+--
+
+DROP TABLE IF EXISTS `TaskVariant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `TaskVariant` (
+  `task` int(11) NOT NULL,
+  `variant` int(11) NOT NULL,
+  KEY `taskvariant_task_idx` (`task`),
+  KEY `taskvariant_category_idx` (`variant`),
+  CONSTRAINT `taskvariant_category` FOREIGN KEY (`variant`) REFERENCES `Variant` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `taskvariant_task` FOREIGN KEY (`task`) REFERENCES `Task` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `User`
 --
 
@@ -26,13 +83,30 @@ DROP TABLE IF EXISTS `User`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `User` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `token` varchar(20) NOT NULL,
+  `email` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `password` varchar(25) CHARACTER SET latin1 NOT NULL,
+  `token` varchar(20) CHARACTER SET latin1 NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `token_UNIQUE` (`token`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `Variant`
+--
+
+DROP TABLE IF EXISTS `Variant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Variant` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `category` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `variant_category_idx` (`category`),
+  CONSTRAINT `variant_category` FOREIGN KEY (`category`) REFERENCES `Category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -44,4 +118,4 @@ CREATE TABLE `User` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-20 21:04:46
+-- Dump completed on 2016-10-23 23:56:01
